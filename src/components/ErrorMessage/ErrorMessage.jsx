6f8a9744c8498f8a1b7166376f1fe0202c939470
Volodymyr-Stupnyk <vstupnik44@gmail.com>
2025-05-01 10:54:24 +0300

    5

--- src/components/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-import css from './ErroeMessage.module.css';
-
-const ErrorMessage = () => {
-    return (
-        <p className={css.errorMessage}>Failed to load images. Please try again later.</p>
-    )
-};
-
-export default ErrorMessage;
